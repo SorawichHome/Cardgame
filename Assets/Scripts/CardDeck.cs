21dc33bdb40995fe53d42e13bbf2dc40605b135d
Yahom93 <sorawich.choti@gmail.com>
2022-06-09 23:10:49 +0700

    Making Deck
    Making deck script that has shuffle too.

---
+++ Assets/Scripts/CardDeck.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CardDeck : MonoBehaviour
+{
+    public List<Card> deck = new List<Card>();
+    public List<Card> Deckcontainer = new List<Card>();
+    public int randomCard;
+    int Decksize = 374;
+    // Start is called before the first frame update
+    void Start()
+    {
+        randomCard = 0;
+        for(int i = 0; i <= Decksize; i++)
+        {
+            randomCard = Random.Range(0, 374);
+            deck.Add(CardDatabase.CardList[randomCard]);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Shuffle()
+    {
+        for(int i = 0; i <= Decksize; i++)
+        {
+            int randomIndex = Random.Range(0, 374);
+            Deckcontainer.Add(CardDatabase.CardList[randomIndex]);
+            deck[i] = deck[randomIndex];
+            deck[randomIndex] = Deckcontainer[0];
+        }
+    }
+}
